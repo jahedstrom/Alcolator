b2ead07ba1b12e6d4ae4e98719e2d19ae84a4149
Jonathan Hedstrom <delnorte91@gmail.com>
2016-06-08 23:49:32 -0500

    Checkpoint 21 complete

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Jonathan on 6/8/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
